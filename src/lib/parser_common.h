148ce6206e6d9fb8ac68173e0cc4fa930adf421f
Juan Leni <juan.leni@zondax.ch>
2019-12-10 17:27:29 +0100

    Cleaning up and unifying error messages (#7)

--- src/lib/parser_common.h
+++ src/lib/parser_common.h
@@ -23,28 +23,28 @@ extern "C" {
 #include <stddef.h>
 
 typedef enum {
+    // Generic errors
     parser_ok = 0,
     parser_no_data,
+    parser_init_context_empty,
+    parser_display_idx_out_of_range,
+    parser_display_page_out_of_range
+    // Coin specific
+    parser_cbor_unexpected,
     parser_unexpected_type,
     parser_unexpected_method,
     parser_unexpected_buffer_end,
     parser_unexpected_value,
     parser_unexpected_number_items,
     parser_unexpected_data_at_end,
-    parser_init_context_empty,
     parser_unexpected_characters,
     parser_unexpected_field,
     parser_value_out_of_range,
-
+    // Context related errors
     parser_context_mismatch,
     parser_context_unexpected_size,
     parser_context_invalid_chars,
     parser_context_unknown_prefix,
-    /////
-    parser_cbor_unexpected,
-    /////
-    parser_display_idx_out_of_range,
-    parser_display_page_out_of_range
 } parser_error_t;
 
 typedef struct {
